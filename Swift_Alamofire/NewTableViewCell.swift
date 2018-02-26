2844a315a46d662500e8e9766687e8bf2f9cda58
Alisha Atlani <Alisha@Alishas-MacBook-Pro.local>
2018-02-26 16:53:42 +0530

    first commit

---
+++ Swift_Alamofire/NewTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewTableViewCell.swift
+//  Swift_Alamofire
+//
+//  Created by Alisha Atlani on 2/26/18.
+//  Copyright © 2018 Alisha Atlani. All rights reserved.
+//
+
+import UIKit
+
+class NewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Swift_Alamofire/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  Swift_Alamofire
+//
+//  Created by Alisha Atlani on 2/26/18.
+//  Copyright © 2018 Alisha Atlani. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
